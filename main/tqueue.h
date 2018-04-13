2e5c399a16768b5b385bb079ed793934539feb48
robertotrapletti <roberto.trapletti@gmail.com>
2018-04-13 14:53:37 +0200

    Example

--- main/tqueue.h
+++ main/tqueue.h
@@ -1,9 +1,8 @@
-//
-// Created by Roberto Trapletti on 15.03.18.
-//
+#ifndef __TQUEUE_H__
+#define __TQUEUE_H__
 
-#ifndef UPSIDEDOWN_TQUEUE_H
-#define UPSIDEDOWN_TQUEUE_H
+#include <stdlib.h>
+#include <stdio.h>
 
 struct TQueueNode;
 typedef struct TQueueNode* TQueue;
@@ -11,18 +10,17 @@ typedef struct TQueueNode* TQueue;
 /* Adds a new element at the end of the list, returns its position */
 unsigned long int tqueue_enqueue(TQueue* q, void* data);
 
-/* Removes and returns the element at the beginning of the list, NULL if the
-is empty */
+/* Removes and returns the element at the beginning of the list, NULL if the queue is empty */
 void* tqueue_pop(TQueue* q);
 
 /* Returns the number of elements in the list */
 unsigned long int tqueue_size(TQueue q);
 
 /* Returns a 'view' on the list starting at (a positive) offset distance,
-* NULL if the queue is empty */
+ * NULL if the queue is empty */
 TQueue tqueue_at_offset(TQueue q, unsigned long int offset);
 
 /* Returns the data on the first node of the given list */
 void* tqueue_get_data(TQueue q);
 
-#endif //UPSIDEDOWN_TQUEUE_H
+#endif
