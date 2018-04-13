3edffac92440fcb0db0e7d1a692b815f020a6e76
robertotrapletti <roberto.trapletti@gmail.com>
2018-04-13 16:04:21 +0200

    Example

--- main/tqueue.c
+++ main/tqueue.c
@@ -1,82 +1,79 @@
-//
-// Created by Roberto Trapletti on 15.03.18.
-//
-
-#include <stddef.h>
-#include <stdlib.h>
-#include "tqueue.h";
+#include "tqueue.h"
 
 typedef struct TQueueNode {
     struct TQueueNode* next;
     void* data;
 } TQueueNode;
 
-/* Adds a new element at the end of the list, returns its position */
-unsigned long int tqueue_enqueue(TQueue* q, void* data){
-
-    if(q==NULL){
-        (*q)->data = data;
-        (*q)->next = q;
-        return 0;
-    }else{
-        int pos = 0;
-        TQueueNode* newNode = (TQueueNode*) malloc(sizeof(TQueueNode));
-        newNode->next = q;
-
-        while((*q)->next != q){
-            q = (*q)->next;
-            pos++;
+unsigned long int tqueue_enqueue(TQueue* q, void* data)
+{
+    TQueueNode* new_element = (TQueueNode*) malloc(sizeof(TQueueNode));
+    new_element->data = data;
+    unsigned long int index = 0;
+    if (*q == NULL) {
+        *q = new_element;
+        new_element->next = *q;
+    } else {
+        TQueueNode* head = *q;
+        index = 1;
+        while(head->next != *q) {
+            head = head->next;
+            index++;
         }
-
-        (*q)->next = newNode;
-        return pos++;
+        head->next = new_element;
+        new_element->next = *q;
     }
+    return index;
 }
 
-void* tqueue_pop(TQueue* q){
-    if(q = NULL){
+void* tqueue_pop(TQueue* q)
+{
+    if (*q == NULL) {
         return NULL;
-    }else{
-        void* data = malloc(sizeof(void*));
-        data = (*q)->data;
-        if((*q)->next != q){
-            q = (*q)->next;
-            return data;
-        }else{
-            q = NULL;
-            return data;
+    } else {
+        TQueueNode* old_head = *q;
+        void* ret_val = old_head->data;
+        if (old_head->next == old_head) {
+            *q = NULL;
+        } else {
+            *q = old_head->next;
+            TQueueNode* head = *q;
+            while(head->next != old_head) {
+                head = head->next;
+            }
+            head->next =*q;
         }
+        free(old_head);
+        return ret_val;
     }
 }
 
-/* Returns the number of elements in the list */
-unsigned long int tqueue_size(TQueue q){
-    int pos =0;
-    if(q = NULL){
-        return 0;
-    }else{
-        while(q->next != q){
-            q = q->next;
-            pos++;
-        }
-        return pos;
+TQueue tqueue_at_offset(TQueue q, unsigned long int offset)
+{
+    if (offset < 0 || q == NULL) return NULL;
+    unsigned long int index;
+    TQueueNode* head = q;
+    for(index=0; index < offset; index++) {
+        head = head->next;
+        //if (head == q) return NULL;
     }
+    return head;
 }
 
-TQueue tqueue_at_offset(TQueue q, unsigned long int offset){
-    /* Returns a 'view' on the list starting at (a positive) offset distance,
-* NULL if the queue is empty */
-    if(q = NULL){
-        return NULL;
-    }else{
-        for(int i = 0; i<offset; i++){
-            q = q->next;
-        }
-        return q;
+unsigned long int tqueue_size(TQueue q)
+{
+    unsigned long int size = 1;
+    if (q == NULL) return 0;
+    TQueueNode* head = q;
+    while(head->next != q) {
+        head = head->next;
+        size += 1;
     }
+    return size;
 }
 
-/* Returns the data on the first node of the given list */
-void* tqueue_get_data(TQueue q){
-    return q->data;
+void* tqueue_get_data(TQueue q)
+{
+    return (q == NULL) ? NULL : q->data;
 }
+
