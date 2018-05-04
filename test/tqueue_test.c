6567694458307f42bc1c2ef49e1d2157289cd44c
Deca <andrea.decarlo@student.supsi.ch>
2018-05-04 15:32:04 +0200

    Conding

--- test/tqueue_test.c
+++ test/tqueue_test.c
@@ -2,3 +2,22 @@
 // Created by Roberto Trapletti on 15.03.18.
 //
 
+
+#include <assert.h>
+#include "../main/tqueue.h"
+
+main(){
+
+    TQueue queue = NULL;
+
+    printf("inizio test");
+    assert(tqueue_size(queue) == 0);
+
+    int* data = (int*) malloc(sizeof(int));
+    *data = 5;
+
+    tqueue_enqueue(&queue,data);
+    assert(tqueue_size(queue) == 1);
+    exit(1);
+
+}
