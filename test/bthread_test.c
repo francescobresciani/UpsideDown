83517500786b52cc7e3dde5a7bf3c438cc573859
maura.clerici@student.supsi.ch <maura.clerici@student.supsi.ch>
2018-05-18 15:12:57 +0200

    Test bthread_create finished

--- test/bthread_test.c
+++ test/bthread_test.c
@@ -6,7 +6,7 @@ void bthread_create_test(){
     printf("inizio  di bthread_create_test -> ");
     bthread_t bthread;
     unsigned long tid = bthread_create(&bthread, NULL, printf("bthread creation"), NULL);
-    bthread_get_scheduler()->queue
+    bthread_get_scheduler()->queue;
     printf("fine di bthread_create_test\n");
 }
 
