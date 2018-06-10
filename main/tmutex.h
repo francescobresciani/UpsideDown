5130c42b6b51ecde5fbfedcdbe8c966c6326131f
attilio <gatzu78@hotmail.com>
2018-06-10 11:45:08 +0200

    implementato tmutex.c e .h

---
+++ main/tmutex.h
@@ -0,0 +1,24 @@
+#ifndef UPSIDEDOWN_TMUTEX_H
+#define UPSIDEDOWN_TMUTEX_H
+#include "tqueue.h"
+
+typedef struct {
+    void* owner;
+    TQueue waiting_list;
+} bthread_mutex_t;
+
+// Defined only for "compatibility" with pthread
+typedef struct {} bthread_mutexattr_t;
+
+// attr is ignored
+int bthread_mutex_init(bthread_mutex_t* m, const bthread_mutexattr_t *attr);
+
+int bthread_mutex_destroy(bthread_mutex_t* m);
+
+int bthread_mutex_lock(bthread_mutex_t* m);
+
+int bthread_mutex_trylock(bthread_mutex_t* m);
+
+int bthread_mutex_unlock(bthread_mutex_t* m);
+
+#endif //UPSIDEDOWN_TMUTEX_H
