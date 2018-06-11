e64bcab1f94c182b4a15824bd206c1e1ff9f66e3
Luciano <luciano.dasilvamoreira@student.supsi.ch>
2018-06-11 18:37:52 +0200

    tcondition test giusto

--- test/condition_test.c
+++ test/condition_test.c
@@ -2,22 +2,32 @@
 // Created by user on 11.06.18.
 //
 
+#include <stdio.h>
 #include <stdlib.h>
-#include "bthread.h"
-#include "tmutex.h"
-#include "tsemaphore.h"
+#include "../main/bthread.h"
+#include "../main/tmutex.h"
+#include "../main/tcondition.h"
 
 #define N 10
 
 int stock = 0;
 bthread_mutex_t stock_mutex;
-bthread_sem_t items_in_stock;
-bthread_sem_t space_in_stock;
+bthread_cond_t items_in_stock;
+bthread_cond_t space_in_stock;
 
-void* producer(void* arg) {
+void* producer(void* arg)
+{
+
 for(;;) {
-bthread_sem_wait(&space_in_stock);
+
 bthread_mutex_lock(&stock_mutex);
+
+while (stock == N) {
+
+bthread_cond_wait(&space_in_stock, &stock_mutex);
+
+}
+
 
 if (stock < N) {
 
@@ -35,19 +45,26 @@ bthread_printf("Producer: stock is full\n");
 
 bthread_mutex_unlock(&stock_mutex);
 
-bthread_sem_post(&items_in_stock);
+bthread_cond_signal(&items_in_stock);
 
 }
-}
+ }
 
-void* consumer(void* arg)
+ void* consumer(void* arg)
 {
 
 for(;;) {
 
-bthread_sem_wait(&items_in_stock);
-
 bthread_mutex_lock(&stock_mutex);
+
+
+while (stock == 0) {
+
+bthread_cond_wait(&items_in_stock, &stock_mutex);
+
+}
+
+
 if (stock > 0) {
 
 /* Consume an item */
@@ -64,32 +81,29 @@ bthread_printf("Consumer: stock is empty\n");
 
 bthread_mutex_unlock(&stock_mutex);
 
-bthread_sem_post(&space_in_stock);
+bthread_cond_signal(&space_in_stock);
 
 }
 
 }
-int main(int argc, char *argv[])
-{
-bthread_mutex_init(&stock_mutex, NULL);
-bthread_sem_init(&items_in_stock, 0, 0);
-bthread_sem_init(&space_in_stock, 0, N);
-volatile bthread_t producer1, consumer1, consumer2;
-bthread_create(&producer1, NULL, producer, NULL);
-bthread_create(&consumer1, NULL, consumer, NULL);
 
-bthread_create(&consumer2, NULL, consumer, NULL);
 
-bthread_join(producer1, NULL);
 
-bthread_join(consumer1, NULL);
+int main(int argc, char *argv[]) {
+ /*   bthread_mutex_init(&stock_mutex, NULL);
+    bthread_cond_init(&items_in_stock, NULL);
+    bthread_cond_init(&space_in_stock, NULL);
+    volatile bthread_t producer1, consumer1, consumer2;
+    bthread_create(&producer1, NULL, producer, NULL,0);
+    bthread_create(&consumer1, NULL, consumer, NULL,0);
+    bthread_create(&consumer2, NULL, consumer, NULL,0);
+    bthread_join(producer1, NULL);
+    bthread_join(consumer1, NULL);
+    bthread_join(consumer2, NULL);
+    bthread_cond_destroy(&items_in_stock);
+    bthread_cond_destroy(&space_in_stock);
+    bthread_mutex_destroy(&stock_mutex);
+    printf("Exiting main\n");*/
+    return 0;
 
-bthread_join(consumer2, NULL);
-
-bthread_sem_destroy(&items_in_stock);
-
-bthread_sem_destroy(&space_in_stock);
-bthread_mutex_destroy(&stock_mutex);
-printf("Exiting main\n");
-return 0;
 }
